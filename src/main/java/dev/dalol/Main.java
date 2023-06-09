82011679670fea91e0dcde4090043d1cc41c86cb
DaLoL <dalol.dev@gmail.com>
2023-06-10 00:47:04 +0200

    Commit

--- src/main/java/dev/dalol/Main.java
+++ src/main/java/dev/dalol/Main.java
@@ -21,6 +21,12 @@ import static net.dv8tion.jda.api.entities.Activity.ActivityType.STREAMING;
 public class Main {
     public static void main(String[] args) {
 
+
+        Dotenv dotenv = Dotenv.configure()
+                .directory("src/main/resources")
+                .filename("env")
+                .load();
+
         JDA jda = JDABuilder.createDefault(dotenv.get("TOKEN"))
                 .addEventListeners(new Grundgesetze(), new CasinoAnleitung(), new Creator(), new JoinEvent(), new LinkDisabler(), new Verify(), new UmfragenCommand(), new Update(), new Info(), new YouTubeCommand(), new RWGame(), new LiveCommand())
                 .setStatus(OnlineStatus.ONLINE)
